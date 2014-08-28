35c5a1bdacae7a604a914c4465b8a9bdcd884e42
goblindegook <goblindegook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-28 00:17:10 +0000

    Version 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/fragmentions/tags/1.2.0@974369 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ fragmentions.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Fragmentions
+Version: 1.2.0
+Description: Fragmentions allow linking to document sections on WordPress sites using words or phrases.
+Author: Luís Rodrigues
+Author URI: http://goblindegook.net/
+Plugin URI: https://github.com/goblindegook/fragmentions/
+Text Domain: fragmentions
+Domain Path: /languages
+*/
+
+if (!defined( 'WPINC' )) {
+    die;
+}
+
+if (!class_exists( 'FragmentionsPlugin' )):
+
+class FragmentionsPlugin {
+
+    const VERSION     = '1.2.0';
+
+    const PLUGIN_SLUG = 'fragmentions';
+
+    function __construct () {
+        add_action( 'init', array( $this, 'init' ) );
+    }
+
+    public function init () {
+        load_plugin_textdomain( 'fragmentions', FALSE, basename( plugin_dir_path( dirname( __FILE__ ) ) ) . '/languages/' );
+
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+    }
+
+    public function enqueue_scripts() {
+        wp_enqueue_style( self::PLUGIN_SLUG . '-styles', plugins_url( 'styles/fragmentions.css', __FILE__ ), array(), self::VERSION );
+        wp_enqueue_script( self::PLUGIN_SLUG . '-script', plugins_url( 'scripts/fragmention.js', __FILE__ ), array(), self::VERSION );
+    }
+}
+
+endif;
+
+new FragmentionsPlugin;
