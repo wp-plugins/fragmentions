c7445e45149b6205322521fc1bec96f977c23506
goblindegook <goblindegook@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-26 14:50:43 +0000

    Version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/fragmentions/trunk@1206838 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts/fragmention.js
+++ scripts/fragmention.js
@@ -1,1 +1,98 @@
-"use strict";if(!("fragmention"in window.location))(function(){function e(t,n){for(var r=t.childNodes,i=0,s,o=[];s=r[i];++i){if(s.nodeType===1&&(s.innerText||s.textContent||"").replace(/\s+/g," ").indexOf(n)!==-1){o=o.concat(e(s,n))}}return o.length?o:t}function t(){if(!/e/.test(document.readyState))return;var t=location.href.match(/#((?:#|%23)?)(.+)/)||[0,"",""],n=document.getElementById(t[1]+t[2]),r=decodeURIComponent(t[2]).replace(/\+/g," ").split("  ");location.fragmention=r[0];location.fragmentionIndex=parseFloat(r[1])||0;if(i){i.removeAttribute("fragmention");if(i.runtimeStyle){i.runtimeStyle.windows=i.runtimeStyle.windows}}if(!n&&location.fragmention){var s=e(document,location.fragmention),o=s.length,u=o&&location.fragmentionIndex%o,a=o&&u>=0?u:o+u;i=o&&s[a];if(i){i.scrollIntoView();i.setAttribute("fragmention","");if(i.runtimeStyle){i.runtimeStyle.windows=i.runtimeStyle.windows}}else{i=null}}}location.fragmention=location.fragmention||"";var n="addEventListener",r=n in window?[n,""]:["attachEvent","on"],i;window[r[0]](r[1]+"hashchange",t);document[r[0]](r[1]+"readystatechange",t);t()})();
+// detect native/existing fragmention support
+if (!('fragmention' in window.location)) (function () {
+    // populate fragmention
+    location.fragmention = location.fragmention || '';
+
+    // return first element in scope containing case-sensitive text
+    function getElementsByText(scope, text) {
+        // iterate descendants of scope
+        for (var all = scope.childNodes, index = 0, element, list = []; (element = all[index]); ++index) {
+            // conditionally return element containing visible, whitespace-insensitive, case-sensitive text (a match)
+            if (element.nodeType === 1 && (element.innerText || element.textContent || '').replace(/\s+/g, ' ').indexOf(text) !== -1) {
+                list = list.concat(getElementsByText(element, text));
+            }
+        }
+
+        // return scope (no match)
+        return list.length ? list : scope;
+    }
+
+    function getAnchorableElementByName(fragment) {
+        var elements = document.getElementsByName(fragment), index = -1;
+
+        while (elements[++index] && !/^A(REA)?$/.test(elements[index].nodeName)) {}
+
+        return elements[index];
+    }
+
+    // on dom ready or hash change
+    function onHashChange() {
+        // do nothing if the dom is not ready
+        if (!/e/.test(document.readyState)) return;
+
+        // set location fragmention as uri-decoded text (from href, as hash may be decoded)
+        var
+        id = location.href.match(/#((?:#|%23)?)(.+)/) || [0,'',''],
+        node = document.getElementById(id[1]+id[2]) || getAnchorableElementByName(id[1]+id[2]),
+        match = decodeURIComponent(id[2].replace(/\+/g, ' ')).split('  ');
+
+        location.fragmention = match[0];
+        location.fragmentionIndex = parseFloat(match[1]) || 0;
+
+        // conditionally remove stashed element fragmention attribute
+        if (element) {
+            element.removeAttribute('fragmention');
+
+            // DEPRECATED: trigger style in IE8
+            if (element.runtimeStyle) {
+                element.runtimeStyle.windows = element.runtimeStyle.windows;
+            }
+        }
+
+        // if fragmention exists
+        if (!node && location.fragmention) {
+            var
+            // get all elements containing text (or document)
+            elements = getElementsByText(document, location.fragmention),
+            // get total number of elements
+            length   = elements.length,
+            // get index of element
+            modulus  = length && location.fragmentionIndex % length,
+            index    = length && modulus >= 0 ? modulus : length + modulus;
+
+            // get element
+            element = length && elements[index];
+
+            // if element found
+            if (element) {
+                // scroll to element
+                element.scrollIntoView();
+
+                // set fragmention attribute
+                element.setAttribute('fragmention', '');
+
+                // DEPRECATED: trigger style in IE8
+                if (element.runtimeStyle) {
+                    element.runtimeStyle.windows = element.runtimeStyle.windows;
+                }
+            }
+            // otherwise clear stashed element
+            else {
+                element = null;
+            }
+        }
+    }
+
+    var
+    // DEPRECATED: configure listeners
+    defaultListener = 'addEventListener',
+    addEventListener = defaultListener in window ? [defaultListener, ''] : ['attachEvent', 'on'],
+    // set stashed element
+    element;
+
+    // add listeners
+    window[addEventListener[0]](addEventListener[1] + 'hashchange', onHashChange);
+    document[addEventListener[0]](addEventListener[1] + 'readystatechange', onHashChange);
+
+    onHashChange();
+})();
